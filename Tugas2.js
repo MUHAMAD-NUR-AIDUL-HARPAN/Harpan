11335477afb33d7198463c1af5704b463ae35dcc
MUHAMAD-NUR-AIDUL-HARPAN <114632752+MUHAMAD-NUR-AIDUL-HARPAN@users.noreply.github.com>
2023-10-05 14:34:32 +0700

    Add files via upload

---
+++ Tugas2.js
@@ -0,0 +1,10 @@
+const dataPribadi = {
+    firstName : "Muhamad Nur Aidul",
+    lastName : "Harpan",
+    age : 20,
+    isMarried : "Belum menikah"
+
+}
+console.log(`Nama :  ${dataPribadi.firstName} ${dataPribadi.lastName}`)
+console.log(`Usia : ${dataPribadi.age}`)
+console.log(`${dataPribadi.isMarried}`) 
